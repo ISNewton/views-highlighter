80ef23ecab6b7080590a45f030d26dfe106ebc23
ISNewton <ashrfalhag.11@gmail.com>
2024-01-20 13:28:42 +0200

    feat:match view

--- lua/highlight.lua
+++ lua/highlight.lua
@@ -1,1 +1,26 @@
-print("welcome ")
+vim.api.nvim_set_decoration_provider(0, {
+  on_buf = function(_, bufnr)
+    local decorations = {}
+    local view_strings = {}
+
+    
+
+    for _, line in ipairs(vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)) do
+      -- local view_match = line:match("view%s*%(.-%)")
+      print(line)
+    --   if view_match then
+    --     -- for string in view_match:gmatch("%b\"\"") do
+    --     --   table.insert(view_strings, {
+    --     --     start = #decorations + 1,
+    --     --     finish = #decorations + #string,
+    --     --     hl_group = "ViewString",
+    --     --   })
+    --     -- end
+    --   end
+
+      -- table.insert(decorations, line)
+    end
+
+    -- return decorations, view_strings
+  end,
+})
--- plugin/highlight.lua
+++ plugin/highlight.lua
@@ -4,8 +4,21 @@ vim.api.nvim_create_autocmd("BufEnter", {
   callback = function()
 
     local lines = vim.api.nvim_buf_get_lines(0, 1 , -1 , false)
+ -- view('')
+    for _, line in ipairs(lines) do
+      -- print(k , line)
 
-    print(lines)
+    local matched = line:match("view%('.*'")
+    if matched ~= nil then
+      print(matched)
+      local string_in_view = string.match(matched , '(.)')
+      -- print(string_in_view)
+    end
+    end
   end,
 })
+
+P = function(name)
+  return vim.inspect(name)
+end
 
